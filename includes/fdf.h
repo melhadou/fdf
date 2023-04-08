4a94f8080e3ac01854610b367d4a3acc73774715
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2023-04-08 15:39:03 +0000

    Fix: normed

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,12 +6,14 @@
 /*   By: melhadou <melhadou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 18:05:31 by melhadou          #+#    #+#             */
-/*   Updated: 2023/04/08 15:17:47 by melhadou         ###   ########.fr       */
+/*   Updated: 2023/04/08 15:38:17 by melhadou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
+// used headers
 # include "mlx.h"
 # include "../libs/getnextline/get_next_line.h"
 # include "../libs/libft/libft.h"
@@ -21,11 +23,17 @@
 # include <fcntl.h>
 # include <limits.h>
 # include <math.h>
+
+// COLORS
 # define RED 0xFF0000
 # define GREEN 0x00FF00
 # define WHITE 0xFFFFFF
+
+// Windows size
 # define WINDOW_WIDTH 1900
 # define WINDOW_HEIGHT 1080
+
+// mlx key events
 # define UP_KEY 126
 # define DOWN_KEY 125
 # define LEFT_KEY 123
@@ -33,66 +41,75 @@
 # define PLUS_KEY 69
 # define MINUS_KEY 78
 
-typedef struct	s_data {
+typedef struct t_data
+{
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
-}				t_data;
+}	t_data;
 
 /**
 	* Array with size
 	*/
 
-typedef struct{
-	int	*arr;
+typedef struct t_int
+{
+	int		*arr;
 	size_t	size;
-} int_array;
+}	t_int;
 
 /**
 	* Point Struct
 	*/
-typedef struct{
+typedef struct t_point
+{
 	float	x;
 	float	y;
-	int	color;
-} t_point;
+	int		color;
+}	t_point;
 
 /**
 	* Map of points Struct
 	*/
-typedef struct {
-	t_point **p;
-} t_map;
+typedef struct t_map
+{
+	t_point	**p;
+}	t_map;
 
 /**
 	* Fdf Struct
 	*/
-typedef struct {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	*img;
-	t_map	*map;
-	size_t	col;
-	size_t	row;
-	float	div_x;
-	float	div_y;
-	float zoom;
-} t_fdf;
+typedef struct t_fdf
+{
+	size_t		col;
+	size_t		row;
+	t_data		*img;
+	float		div_x;
+	float		div_y;
+	float		zoom;
+	t_map		*map;
+	void		*mlx;
+	void		*mlx_win;
+}	t_fdf;
 
-int	key_hook(int key, t_fdf *fdf);
-void	rendring(t_fdf *fdf);
-float	sc(t_fdf *fdf);
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
-void	resize_map(t_fdf *fdf);
-void	bresenham(t_fdf *fdf, t_point start, t_point end);
-t_fdf *final_map(int_array **map);
-void	to_Isometric(int x, int y, int z, t_point *p);
-size_t	arr_len(char **arr);
-int_array	*split_line(char *line);
-int_array **parse_map(char **map);
+// reading from map and parsing
 char	**append_to_arr(char **map, char *line);
 char	**read_map(int fd);
+t_int	*split_line(char *line);
+t_int	**parse_map(char **map);
+t_fdf	*final_map(t_int **map);
+size_t	arr_len(char **arr);
+void	resize_map(t_fdf *fdf);
 
-#endif // DEBUG
+// mlx related functions
+int		key_hook(int key, t_fdf *fdf);
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
+
+// drawing functions
+void	rendring(t_fdf *fdf);
+void	bresenham(t_fdf *fdf, t_point start, t_point end);
+void	to_Isometric(int x, int y, int z, t_point *p);
+
+#endif // FDF_H
