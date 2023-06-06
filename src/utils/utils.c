a454877407eb8407a37299903663551b80255de4
Mohamed Elhadouchi <melhadou@e2r2p3.1337.ma>
2023-06-06 15:19:31 +0100

    Add: ft_zoom to utils file

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: melhadou <melhadou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 17:19:14 by melhadou          #+#    #+#             */
-/*   Updated: 2023/05/27 17:36:53 by melhadou         ###   ########.fr       */
+/*   Updated: 2023/06/06 15:16:21 by melhadou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,60 @@ void	ft_frees(t_int **p)
 	while (p[++i])
 		free(p[i]);
 	free(p);
+}
+
+void	free_fdf(t_fdf *res)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	while (i < res->row)
+	{
+		len = 0;
+		while (len < res->col)
+		{
+			free(res->map[i].p[len]);
+			len++;
+		}
+		free(res->map[i].p);
+		i++;
+	}
+	free(res->map);
+	free(res);
+}
+
+int	check_file_extension(char *file_name)
+{
+	int i;
+	int len;
+
+	if (!file_name)
+		return (0);
+	i = 0;
+	len = ft_strlen(file_name);
+	while (file_name[len] && i != 4)
+	{
+		if (ft_strcmp(".fdf", ft_substr(file_name, len - 4, 4)))
+			return (1);
+		i++;
+		len--;
+	}
+	return (1);
+}
+
+int	ft_zome(t_fdf *fdf)
+{
+	int    nbx;
+	int    nby;
+	int    zome;
+
+	nbx = 0;
+	nby = 0;
+	while (nbx * fdf->col < WINDOW_WIDTH)
+		nbx++;
+	while (nby * fdf->row < WINDOW_HEIGHT)
+		nby++;
+	zome = (nbx + nby) / 6 + 1;
+	return (zome);
 }
